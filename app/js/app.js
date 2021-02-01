a26fe942067c565e8cd7522a1655318049275927
Michael Newman <mikeronewman@gmail.com>
2021-02-01 13:00:50 -0500

    add hamburger menu animations on click

---
+++ app/js/app.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+
+hamburger.onclick = function() {
+    hamburger.classList.toggle('open');
+}
---
+++ dist/all.js
@@ -0,0 +1,1 @@
+const hamburger=document.getElementById("hamburger");hamburger.onclick=function(){hamburger.classList.toggle("open")};
